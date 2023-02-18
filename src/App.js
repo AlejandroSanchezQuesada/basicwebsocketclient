0191817e840fffac97d9efd372ed9690a195888b
AlejandroSanchezQuesada <sanchezquesadaalejandro@gmail.com>
2023-02-18 17:58:40 +0100

    feat: pruebas chatparaguero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React, { useState } from 'react';
 import SockJsClient from 'react-stomp';
+import axios from 'axios';
+import TextMessageDTO from './textMessajeDTO.ts';
 
-const SOCKET_URL = 'http://localhost:8085/chatparaguero';
+const SOCKET_URL = 'http://localhost:8085/chatspring';
+const AXIOS_URL = 'http://localhost:8085';
 
 const App = () => {
   const [message, setMessage] = useState('You server message here.');
+  const [listMessages, setListMessages] = useState([]);
+
+  const [inputMessage, setInputMessage] = useState('');
+  const [user, setUser] = useState('');
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let textMessageDTO = new TextMessageDTO(user, inputMessage);
+    console.log("textMessageDTO", textMessageDTO);
+
+
+    axios.post(AXIOS_URL + '/send', textMessageDTO).then(res => {
+      console.log("res", res);
+      console.log("res.data", res.data);
+    }).catch(err => {
+      console.log("err", err);
+    }
+    );
+
+  }
+
 
   let onConnected = () => {
     console.log("Connected!!")
@@ -12,10 +38,12 @@ const App = () => {
 
   let onMessageReceived = (msg) => {
     setMessage(msg.message);
+    listMessages.push(msg.message);
+    setListMessages(listMessages);
   }
 
   return (
-    <div>
+    <div >
       <SockJsClient
         url={SOCKET_URL}
         topics={['/topic/message']}
@@ -24,7 +52,21 @@ const App = () => {
         onMessage={msg => onMessageReceived(msg)}
         debug={false}
       />
-      <div>{message}</div>
+      <h1>Paco Chat</h1>
+      <h1>--------------</h1>
+      <ul>
+        {listMessages.map((message, index) => {
+          return <li key={index}>{message}</li>
+        })}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={user} onChange={e => setUser(e.target.value)} defaultValue={"Usuario"} />
+        <input type="text" value={inputMessage} onChange={e => setInputMessage(e.target.value)} />
+
+        <input type="submit" />
+      </form>
+
+
     </div>
   );
 }
---
+++ src/textMessajeDTO.ts
@@ -0,0 +1,16 @@
+
+export default class TextMessageDTO {
+
+    userName: Text;
+    message: Text;
+
+    constructor(userName: Text, message: Text) {
+        this.userName = userName;
+        this.message = message;
+    }
+
+    
+ 
+
+}
+

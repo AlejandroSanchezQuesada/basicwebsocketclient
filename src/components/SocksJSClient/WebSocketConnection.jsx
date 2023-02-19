d6f20067d6cd6cc661e1b43e3b66609f340f8308
AlejandroSanchezQuesada <sanchezquesadaalejandro@gmail.com>
2023-02-19 14:21:13 +0100

    fix: refactor

---
+++ src/components/SocksJSClient/WebSocketConnection.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import SockJsClient from 'react-stomp';
+import envDev from '../../envDev.json';
+
+export const WebSocketConnection = (props) => {
+
+  const SOCKET_URL = envDev.SOCKET_URL;
+
+  let onConnected = () => {
+    console.log("Connected!!")
+  }
+
+  let onMessageReceived = (msg) => {
+    console.log("New Message Received!!", msg);
+    props.nuevoMensaje(msg.message);
+  }
+
+  return (
+    <SockJsClient
+      url={SOCKET_URL}
+      topics={['/topic/message']}
+      onConnect={onConnected}
+      onDisconnect={console.log("Disconnected!")}
+      onMessage={(msg) => onMessageReceived(msg)}
+      debug={false}
+    />
+  );
+};
+
+
